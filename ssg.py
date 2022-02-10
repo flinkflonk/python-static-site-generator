51d9b7e3b07fe1d4dbfc5d1bb5954427617511d7
Michael Hinz <michael@hinz.no>
2022-02-10 17:15:22 +0100

    fix(module1): make it pass all tests

---
+++ ssg.py
@@ -0,0 +1,13 @@
+import typer
+from ssg.site import Site
+
+def main(source="content", dest="dist"):
+    config = {
+        'source': source,
+        'dest': dest,
+    }
+    
+    Site(**config).build()
+
+if __name__ == '__main__':
+    typer.run(main)
